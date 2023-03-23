18d771ef4fd285482ab931654fe7c0ccad266735
mackwafang <mackvu67@gmail.com>
2023-03-23 11:15:15 -0700

    moved wonton file to proper place

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from mackbot.utilities.compile_bot_help import compile_help_strings
 from cogs import *
 from cogs.wows.listener import Listener
 
-class Mackbot(commands.Bot):
+class Mackbot(discord.Client):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 
@@ -23,7 +23,11 @@ bot_intents.members = True
 bot_intents.typing = True
 bot_intents.message_content = True
 
-mackbot = Mackbot(command_prefix=commands.when_mentioned_or(command_prefix), intents=bot_intents, help_command=None)
+mackbot = Mackbot(
+	# command_prefix=commands.when_mentioned_or(command_prefix),
+	intents=bot_intents,
+	help_command=None
+)
 
 # create temp directory for bot
 if not os.path.isdir(os.path.join(".", "tmp")):
@@ -41,7 +45,7 @@ async def main():
 	if database_client is None:
 		load_ship_builds()
 
-	await mackbot.load_extension("mackbot.misc_commands.wtn")
+	await mackbot.load_extension("mackbot.misc.wtn")
 	await mackbot.add_cog(Listener(mackbot, command_prefix))
 	for cog in BOT_COGS:
 		await mackbot.add_cog(cog(mackbot))
